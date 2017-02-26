924bfa6357b13144eab051cb5d8a07a6e3cbd52a
Ning Wang <Nw265@cornell.edu>
2017-02-26 18:05:19 -0500

    local db connection and setup

---
+++ src/main/java/database/LocalMySQL.java
@@ -0,0 +1,24 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * Functions related to local MySQL database.
+ * @author Ning
+ *
+ */
+public class LocalMySQL {
+	
+    private static Connection conn;
+    public static Connection getConnection(String database, String user, String pwd) {
+    	try {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + database, user, pwd);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+		return conn;
+    }
+    
+}
