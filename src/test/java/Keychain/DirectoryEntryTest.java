e7e126e928e769b828f7811970f966c5767dced6
Nishad Mathur <nishadmathur@gmail.com>
2017-02-26 13:54:39 -0500

    Create test skeletons.

---
+++ src/test/java/Keychain/DirectoryEntryTest.java
@@ -0,0 +1,27 @@
+package Keychain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by nishadmathur on 24/2/17.
+ */
+public class DirectoryEntryTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void dump() throws Exception {
+
+    }
+
+    @Test
+    public void resolve() throws Exception {
+
+    }
+
+}
---
+++ src/test/java/Keychain/KeychainTest.java
@@ -0,0 +1,22 @@
+package Keychain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by nishadmathur on 24/2/17.
+ */
+public class KeychainTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void dump() throws Exception {
+
+    }
+
+}
