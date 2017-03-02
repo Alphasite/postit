eacec3eef4cab998ab188465412d99b88d6f56c9
Ning Wang <Nw265@cornell.edu>
2017-03-02 15:06:35 -0500

    updated database schema and initialization data

--- database/init_schema.sql
+++ database/init_schema.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `pwddb` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `pwddb`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: pwddb
@@ -46,6 +44,89 @@ LOCK TABLES `account` WRITE;
 INSERT INTO `account` VALUES ('ning','5431','nw265@cornell.edu','Ning','Wang');
 /*!40000 ALTER TABLE `account` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `directory`
+--
+
+DROP TABLE IF EXISTS `directory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `directory` (
+  `user_name` varchar(25) NOT NULL,
+  `own_path` varchar(45) NOT NULL,
+  `directory_id` varchar(45) NOT NULL,
+  PRIMARY KEY (`user_name`,`directory_id`),
+  UNIQUE KEY `user_name_UNIQUE` (`user_name`),
+  UNIQUE KEY `directory_id_UNIQUE` (`directory_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `directory`
+--
+
+LOCK TABLES `directory` WRITE;
+/*!40000 ALTER TABLE `directory` DISABLE KEYS */;
+INSERT INTO `directory` VALUES ('ning','.','ning');
+/*!40000 ALTER TABLE `directory` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `directory_entry`
+--
+
+DROP TABLE IF EXISTS `directory_entry`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `directory_entry` (
+  `directory_entry_id` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `encryption_key` varchar(45) NOT NULL,
+  `mac` varchar(45) NOT NULL,
+  `permission` varchar(45) NOT NULL,
+  `nonce` varchar(45) NOT NULL,
+  `directory_id` varchar(45) NOT NULL,
+  PRIMARY KEY (`directory_entry_id`,`directory_id`),
+  UNIQUE KEY `directory_entry_id_UNIQUE` (`directory_entry_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `directory_entry`
+--
+
+LOCK TABLES `directory_entry` WRITE;
+/*!40000 ALTER TABLE `directory_entry` DISABLE KEYS */;
+INSERT INTO `directory_entry` VALUES ('pwd1','pwd','lalalalala','la','0','lala','ning'),('pwd2','pwd2','hahahaha','ha','0','haha','ning');
+/*!40000 ALTER TABLE `directory_entry` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `keychain`
+--
+
+DROP TABLE IF EXISTS `keychain`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `keychain` (
+  `directory_entry_id` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `metadata` varchar(45) NOT NULL,
+  PRIMARY KEY (`directory_entry_id`),
+  UNIQUE KEY `directory_entry_id_UNIQUE` (`directory_entry_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `keychain`
+--
+
+LOCK TABLES `keychain` WRITE;
+/*!40000 ALTER TABLE `keychain` DISABLE KEYS */;
+INSERT INTO `keychain` VALUES ('pwd1','5431','nothing'),('pwd2','5430','something');
+/*!40000 ALTER TABLE `keychain` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -56,4 +137,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-02-26 16:38:20
+-- Dump completed on 2017-03-02 15:05:59
--- src/test/java/database/DatabaseConnectionTest.java
+++ src/test/java/database/DatabaseConnectionTest.java
@@ -15,18 +15,50 @@ public class DatabaseConnectionTest {
 
 			Statement st = conn.createStatement();
 
-			ResultSet rs = st.executeQuery("select * from account");
+			ResultSet rs1 = st.executeQuery("select * from account");
 
-			if(rs.next()) {
+			if(rs1.next()) {
 
-				String name = rs.getString(1);
+				String name = rs1.getString("user_name");
 
-				String key = rs.getString(2);
+				String key = rs1.getString("pwd_key");
+				
+				System.out.printf("username: %s\tkey: %s\n", name, key);
+				
+				ResultSet rs2 = st.executeQuery("select * from directory where user_name='" + name + "'");
+				
+				String directoryid = "";
+				if (rs2.next()){
+					directoryid = rs2.getString("directory_id");
+					System.out.println("\t" + "directory: " + directoryid);
+				
+					ResultSet rs3 = st.executeQuery("select * from directory_entry where directory_id='" + directoryid + "'");
+					while (rs3.next()){
+						String deid = rs3.getString("directory_entry_id");
+						String dename = rs3.getString("name");
+						String dekey = rs3.getString("encryption_key");
+						
+						Statement st2 = conn.createStatement();
+						ResultSet rs4 = st2.executeQuery("select * from keychain where directory_entry_id ='" + deid + "'");
+						String pwd = "";
+						String data = "";
+						if (rs4.next()){
+							pwd = rs4.getString("password");
+							data = rs4.getString("metadata");
+						}
+						
+						rs4.close();
+						System.out.printf("\t\tid: %s, name: %s, key: %s -- pwd: %s, metadata: %s\n", deid, dename, dekey, pwd, data);
+					}
+					
+					rs3.close();
+				}
 
-				System.out.println(name+"\t"+key);
+				
+				rs2.close();
 			}           
 
-			rs.close();
+			rs1.close();
 
 			st.close();
 			conn.close();
