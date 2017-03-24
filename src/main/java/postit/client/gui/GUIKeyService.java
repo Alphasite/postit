903bab13025c13619f798c4276efa655a98b954a
Ning Wang <Nw265@cornell.edu>
2017-03-24 00:56:01 -0400

    public constructor... may not be correct

--- src/main/java/postit/client/gui/GUIKeyService.java
+++ src/main/java/postit/client/gui/GUIKeyService.java
@@ -21,7 +21,7 @@ public class GUIKeyService implements KeyService {
 
     private ServerController sc;
 
-    GUIKeyService(){
+    public GUIKeyService(){
     }
 
     GUIKeyService(ServerController sc){
