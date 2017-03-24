db44d4ea09557091ad980fd94f6f3cdb02ccea38
Nishad Mathur <nishadmathur@gmail.com>
2017-03-23 20:09:09 -0400

    Renamed test file.

--- src/test/java/postit/client/ClientCommunicationTest.java
+++ src/test/java/postit/client/ClientCommunicationTest.java
@@ -12,83 +12,83 @@ import postit.client.controller.RequestMessenger;
 import postit.communication.*;
 
 
-public class ClientCommunication implements Runnable {
-	ClientSender s;
-	ClientReceiver r;
+public class ClientCommunicationTest implements Runnable {
+    ClientSender s;
+    ClientReceiver r;
 
-	public static JsonObject stringToJsonObject(String msg){
-		JsonReader jsonReader = Json.createReader(new StringReader(msg));
-		JsonObject object = jsonReader.readObject();
-		jsonReader.close();
-		return object;
-	}
+    public static JsonObject stringToJsonObject(String msg) {
+        JsonReader jsonReader = Json.createReader(new StringReader(msg));
+        JsonObject object = jsonReader.readObject();
+        jsonReader.close();
+        return object;
+    }
 
-	public static void testRequest(String request, ClientSender sender, ClientReceiver listener){
-		int reqId = sender.addRequest(request);
-		System.out.println("Sending to server: " + request);
-		String response = null;
-		while(true){ // block until request is received
-			try {
-				Thread.sleep(2000);
-				//System.out.println("sleeping.....");
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			response = listener.getResponse(reqId);
-			if (response != null)
-				break;
-		}
+    public static void testRequest(String request, ClientSender sender, ClientReceiver listener) {
+        int reqId = sender.addRequest(request);
+        System.out.println("Sending to server: " + request);
+        String response = null;
+        while (true) { // block until request is received
+            try {
+                Thread.sleep(2000);
+                //System.out.println("sleeping.....");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            response = listener.getResponse(reqId);
+            if (response != null)
+                break;
+        }
 
-		JsonObject res = stringToJsonObject(response);
-		System.out.println("Response: " + response);
-	}
+        JsonObject res = stringToJsonObject(response);
+        System.out.println("Response: " + response);
+    }
 
-	public ClientCommunication(ClientSender s, ClientReceiver r){
-		this.s = s;
-		this.r = r;
-	}
+    public ClientCommunicationTest(ClientSender s, ClientReceiver r) {
+        this.s = s;
+        this.r = r;
+    }
 
-	@Override
-	public void run(){
-		String req = RequestMessenger.createAuthenticateMessage("ning", "5431");
-		testRequest(req, s, r);
-		req = RequestMessenger.createAuthenticateMessage("ning", "5430");
-		testRequest(req, s, r);
-		req = RequestMessenger.createAuthenticateMessage("mc", "5431");
-		testRequest(req, s, r);
-		req = RequestMessenger.createAddKeychainsMessage("ning", "fb", "hihi", "1234", "");
-		testRequest(req, s, r);
-		req = RequestMessenger.createUpdateKeychainMessage("ning", "fb", "lala", "4321", "nothing");
-		testRequest(req, s, r);
-		req = RequestMessenger.createUpdateKeychainMessage("ning", "net", "lala", "4321", "nothing");
-		testRequest(req, s, r);
-		req = RequestMessenger.createGetKeychainsMessage("ning");
-		testRequest(req, s, r);
-		req = RequestMessenger.createRemoveKeychainMessage("ning", "net");
-		testRequest(req, s, r);
-		req = RequestMessenger.createRemoveKeychainMessage("ning", "fb");
-		testRequest(req, s, r);
-		req = RequestMessenger.createGetKeychainsMessage("ning");
-		testRequest(req, s, r);
-	}
+    @Override
+    public void run() {
+        String req = RequestMessenger.createAuthenticateMessage("ning", "5431");
+        testRequest(req, s, r);
+        req = RequestMessenger.createAuthenticateMessage("ning", "5430");
+        testRequest(req, s, r);
+        req = RequestMessenger.createAuthenticateMessage("mc", "5431");
+        testRequest(req, s, r);
+        req = RequestMessenger.createAddKeychainsMessage("ning", "fb", "hihi", "1234", "");
+        testRequest(req, s, r);
+        req = RequestMessenger.createUpdateKeychainMessage("ning", "fb", "lala", "4321", "nothing");
+        testRequest(req, s, r);
+        req = RequestMessenger.createUpdateKeychainMessage("ning", "net", "lala", "4321", "nothing");
+        testRequest(req, s, r);
+        req = RequestMessenger.createGetKeychainsMessage("ning");
+        testRequest(req, s, r);
+        req = RequestMessenger.createRemoveKeychainMessage("ning", "net");
+        testRequest(req, s, r);
+        req = RequestMessenger.createRemoveKeychainMessage("ning", "fb");
+        testRequest(req, s, r);
+        req = RequestMessenger.createGetKeychainsMessage("ning");
+        testRequest(req, s, r);
+    }
 
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		// FOR CONNECTING TO THE POSTIT SERVER
-		ClientSender sender = new ClientSender(2048);
-		ClientReceiver listener = new ClientReceiver(4880);
+        // FOR CONNECTING TO THE POSTIT SERVER
+        ClientSender sender = new ClientSender(2048);
+        ClientReceiver listener = new ClientReceiver(4880);
 
-		Thread t1 = new Thread(listener);
-		Thread t2 = new Thread(sender);
+        Thread t1 = new Thread(listener);
+        Thread t2 = new Thread(sender);
 
-		t2.start();
-		t1.start();
+        t2.start();
+        t1.start();
 
-		ClientCommunication client = new ClientCommunication(sender, listener);
-		Thread t3 = new Thread(client);
-		final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
-		executor.schedule(t3, 5, TimeUnit.SECONDS);
+        ClientCommunicationTest client = new ClientCommunicationTest(sender, listener);
+        Thread t3 = new Thread(client);
+        final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
+        executor.schedule(t3, 5, TimeUnit.SECONDS);
 
-	}
+    }
 }
