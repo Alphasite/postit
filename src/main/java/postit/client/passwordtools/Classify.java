9d98d24d1946588e366bea5f989a3057e84774cd
Zhan Zhao <zhanzhao@Zhans-MacBook-Pro.local>
2017-04-18 11:29:56 -0400

    modified classify

--- src/main/java/postit/client/passwordtools/Classify.java
+++ src/main/java/postit/client/passwordtools/Classify.java
@@ -1,14 +1,46 @@
-package postit.shared;
+package postit.client.passwordtools;
+
+import org.json.JSONObject;
 
 import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
 
 /**
  * Created by Zhan on 3/23/2017.
  */
+
 public class Classify {
+
+    final String dictionaryWords = "Contains dictionary words";
+    final String comp8 = "Lacks upper case/lower case/symbol/digit";
+    final String bas16 = "Longer password, at least 16 characters";
+
+    public static class Result {
+        private String evaluation;
+        private String strength;
+
+        public Result (String evaluation, Level strength) {
+            this.evaluation = evaluation;
+            this.strength = strength.name();
+        }
+
+        public String getEvaluation () {
+            return evaluation;
+        }
+
+        public String getStrength () {
+            return strength;
+        }
+
+        private JSONObject toObject () {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("evaluation", getEvaluation());
+            jsonObject.put("strength", getStrength());
+
+            return jsonObject;
+        }
+    }
 
     public enum Level {
         HIGH,
@@ -16,9 +48,6 @@ public class Classify {
         LOW,
         ERROR
     }
-
-    public static final String WEAK = "weak";
-    public static final String STRONG = "strong";
 
     File wordlist = Paths.get("").resolve("src").resolve("main").resolve("resources").
             resolve("passwordStrength").resolve("password-2011.lst").toFile();
@@ -75,31 +104,46 @@ public class Classify {
         return !checkDicWords(password, wordlist);
     }
 
-    public Level strengthCheck (String password){
-        //TODO: modify to get more levels of strength
+    public JSONObject strengthCheck (String password){
         try {
-            if ((comprehensive8(password, wordlist) && blacklistHard(password, wordlist2))
-                    || basic16(password)){
-                return Level.HIGH;
+            // basic16 is the highest strength, c8 and blacklist hard is medium,
+            // everything else is low
+            boolean c8 = comprehensive8(password, wordlist);
+            boolean bListHard = blacklistHard(password, wordlist2);
+
+            if (basic16(password)){
+                return new Result("", Level.HIGH).toObject();
             }
-            else
-                return Level.LOW;
+            else if (c8 && bListHard) {
+                return new Result(bas16, Level.MEDIUM).toObject();
+            }
+            else {
+                if (!c8 && !bListHard) {
+                    return new Result(bas16 + " " + comp8, Level.LOW).toObject();
+                }
+                if (!c8) {
+                    return new Result(bas16 + " " + comp8, Level.LOW).toObject();
+                }
+
+                return new Result(bas16 + " " + dictionaryWords, Level.LOW).toObject();
+
+            }
         } catch (Exception e) {
             e.printStackTrace();
-            return Level.ERROR;
+            return new Result(Level.ERROR.name(), Level.ERROR).toObject();
         }
     }
 
-//    public static void main (String[] args) {
-//        Path curr = Paths.get("");
-//        File file = Paths.get("").resolve("src").resolve("main").resolve("resources").
-//                resolve("passwordStrength").resolve("password-2011.lst").toFile();
-//        System.out.println(curr.toAbsolutePath().toString());
-//        try {
-//            BufferedReader in = new BufferedReader(new FileReader(file));
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
+    public static void main (String[] args) {
+        Path curr = Paths.get("");
+        File file = Paths.get("").resolve("src").resolve("main").resolve("resources").
+                resolve("passwordStrength").resolve("password-2011.lst").toFile();
+        System.out.println(curr.toAbsolutePath().toString());
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(file));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
