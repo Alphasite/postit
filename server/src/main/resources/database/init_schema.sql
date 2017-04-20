1a9363d161055f30fb7995454248038b407e87db
Nishad Mathur <nishadmathur@gmail.com>
2017-04-20 11:50:50 -0400

    Fix typos create script.

--- server/src/main/resources/database/init_schema.sql
+++ server/src/main/resources/database/init_schema.sql
@@ -54,7 +54,7 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `login`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `account` (
+CREATE TABLE `login` (
   `time` datetime(6) null,
   `user_name` varchar(45) null,
   `status` tinyint null,
