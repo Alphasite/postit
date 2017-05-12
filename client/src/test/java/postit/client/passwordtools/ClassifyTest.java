64e84634821b52f1138d83cd47f6e4bdadd9c25f
Nishad <nishadmathur@gmail.com>
2017-05-12 16:47:13 -0400

    Added skeleton tests.

---
+++ client/src/test/java/postit/client/passwordtools/ClassifyTest.java
@@ -0,0 +1,33 @@
+package postit.client.passwordtools;
+
+import org.junit.Test;
+
+/**
+ * Created by nishadmathur on 12/5/17.
+ */
+public class ClassifyTest {
+    @Test
+    public void checkDicWords() throws Exception {
+    }
+
+    @Test
+    public void basic16() throws Exception {
+    }
+
+    @Test
+    public void comprehensive8() throws Exception {
+    }
+
+    @Test
+    public void blacklistHard() throws Exception {
+    }
+
+    @Test
+    public void strengthCheck() throws Exception {
+    }
+
+    @Test
+    public void isWeak() throws Exception {
+    }
+
+}
